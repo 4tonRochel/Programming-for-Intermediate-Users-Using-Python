957f63795cbfd52541177d7f98beb9e1975fa0bd
4tonRochel <furtonrochel@gmail.com>
2022-05-27 14:25:48 +0800

    Coding Exercise 4.4

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+from functionfile import personal_greeting
+from functionfile import your_province
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+personal_greeting("Anna")
+your_province("Pampanga")
